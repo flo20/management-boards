d2930a60e048e35aa026e816d3ca750711163b0f
Flowry <flowry@Flowrys-MacBook-Pro.local>
2021-02-05 16:02:05 +0200

    Search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Search from "./components/Search/Search";
+import "./App.scss";
 
 const App = () => {
   return (
     <div>
-      Testing
+      <Search/>
     </div>
   );
 };
 
-export default App
+export default App;
---
+++ src/components/Search/Search.js
@@ -0,0 +1,51 @@
+import React, { Fragment,useState } from "react";
+
+//data for to be searched
+const data = [
+  "Scenario Name   1",
+  "Scenario Name   2",
+  "Scenario Name   3",
+  "Scenario Name   4",
+  "Scenario Name   5",
+  "Scenario Name   6",
+  "Scenario Name   7",
+  "Scenario Name   8",
+  "Scenario Name   9", 
+  "Scenario Name   10" 
+];
+
+const Search = () => {
+  //state for searchValue
+  const [searchValue, setSearchValue] = useState("");
+
+  //update the search value
+  const handleSearchChange = (e)=> {
+    setSearchValue(e.target.value);
+  };
+
+  //filter results
+
+  const results = !searchValue ? data : data.filter((dat)=> dat.toLowerCase().includes(searchValue.toLocaleLowerCase()));
+
+  return (
+    <Fragment>
+      <div>
+        <input type="text" placeholder="Search Scenario Name" value={searchValue} onChange={handleSearchChange}/>
+        <ul>
+          {
+            results.map((item)=>{
+              return(
+                <li key={item}>
+                  {item}
+                </li>      
+              );
+            }) 
+          }
+                   
+        </ul>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Search;
